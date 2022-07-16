a4007cbbe4167bf079cfd84ca3a3d17d78e724de
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-07-16 13:48:06 -0300

    newStyle PhotoDeleteButton

--- src/components/Photo/PhotoDelete.js
+++ src/components/Photo/PhotoDelete.js
@@ -2,22 +2,27 @@ import React from 'react'
 import { PHOTO_DELETE } from '../../api'
 import useFetch from '../Hooks/useFetch'
 import styles from './PhotoDelete.module.css'
+import { useNavigate } from 'react-router-dom'
 
 const PhotoDelete = ({ id }) => {
     const { request, loading } = useFetch()
 
-    const handleClickDelete = async (e) => {
-        e.preventDefault();
-        const { url, options } = PHOTO_DELETE(id);
-        const response = await request(url, options);
-        if (response.response.status === 200) {
-            window.location.reload()
+    const navigate = useNavigate()
+
+    const handleClickDelete = async () => {
+        const confirm = window.confirm('Tem certeza que deseja deletar?')
+        if (confirm) {
+            const { url, options } = PHOTO_DELETE(id);
+            const response = await request(url, options);
+            console.log(response.response.status)
+            if (response.response.status === 200) navigate('/')
         }
+
     }
 
     return (
         <>
-            <button onClick={handleClickDelete} className={styles.delete}>Deletar</button>
+            {loading ? <button className={styles.delete} disabled>Deletando...</button> : <button onClick={handleClickDelete} className={styles.delete}>Deletar</button>}
         </>
     )
 }
