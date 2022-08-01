560b590b231c058146ebfd4036a04ce729ddf73c
Rafael Jordão <rafaeljordao.picado@gmail.com>
2022-08-01 12:38:17 -0300

    newFeature LoginPasswordLost

--- src/App.js
+++ src/App.js
@@ -21,9 +21,7 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />}/>
             <Route path="login/*" element={<Login />}/>
-           
             <Route path="conta/*" element={<ProtectedRoute><User /></ProtectedRoute>}/>
-
             <Route path="foto/:id" element={<Photo />}/>
             <Route path="perfil/:user" element={<UserProfile />}/>
             <Route path="*" element={<NotFound />}/>
